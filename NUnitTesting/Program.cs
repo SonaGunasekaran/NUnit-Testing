70b213f4679708cdebe2834af9a8ad2298d1d5e4
[Sona] <[sona16061999@gmail.com]>
2021-07-02 13:44:57 +0530

    Initialized with welcome Message

---
+++ NUnitTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the N Unit Testing Programs");
+        }
+    }
+}
